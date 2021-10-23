354ac014d54f59f9c22ced8b9f688eed1e798660
Nathan Douglas <github@darkdell.net>
2021-10-23 15:20:45 -0400

    Chapter 4.10.

--- wasm/downdelving.js
+++ wasm/downdelving.js
@@ -793,16 +793,16 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1133 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 261, __wbg_adapter_24);
+    imports.wbg.__wbindgen_closure_wrapper1201 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 273, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1591 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 505, __wbg_adapter_27);
+    imports.wbg.__wbindgen_closure_wrapper1608 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 510, __wbg_adapter_27);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1593 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 505, __wbg_adapter_30);
+    imports.wbg.__wbindgen_closure_wrapper1610 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 510, __wbg_adapter_30);
         return addHeapObject(ret);
     };
 
