aa08fed2202b8c2ef2e63f27ece11cd0c80ae51f
Nathan Douglas <github@darkdell.net>
2021-10-23 15:57:55 -0400

    Chapter 4.17.

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -24,6 +24,9 @@ mod rooms_corridors_dogleg;
 mod rooms_corridors_bsp;
 mod room_sorter;
 mod room_draw;
+mod rooms_corridors_nearest;
+mod rooms_corridors_lines;
+mod room_corridor_spawner;
 use distant_exit::DistantExit;
 use simple_map::SimpleMapBuilder;
 use bsp_dungeon::BspDungeonBuilder;
@@ -48,12 +51,16 @@ use rooms_corridors_dogleg::DoglegCorridors;
 use rooms_corridors_bsp::BspCorridors;
 use room_sorter::{RoomSorter, RoomSort};
 use room_draw::RoomDrawer;
+use rooms_corridors_nearest::NearestCorridors;
+use rooms_corridors_lines::StraightLineCorridors;
+use room_corridor_spawner::CorridorSpawner;
 
 pub struct BuilderMap {
     pub spawn_list : Vec<(usize, String)>,
     pub map : Map,
     pub starting_position : Option<Position>,
     pub rooms: Option<Vec<Rect>>,
+    pub corridors: Option<Vec<Vec<usize>>>,
     pub history : Vec<Map>
 }
 
@@ -85,6 +92,7 @@ impl BuilderChain {
                 map: Map::new(new_depth),
                 starting_position: None,
                 rooms: None,
+                corridors: None,
                 history : Vec::new()
             }
         }
@@ -173,10 +181,17 @@ fn random_room_builder(rng: &mut rltk::RandomNumberGenerator, builder : &mut Bui
 
         builder.with(RoomDrawer::new());
 
-        let corridor_roll = rng.roll_dice(1, 2);
+        let corridor_roll = rng.roll_dice(1, 4);
         match corridor_roll {
             1 => builder.with(DoglegCorridors::new()),
+            2 => builder.with(NearestCorridors::new()),
+            3 => builder.with(StraightLineCorridors::new()),
             _ => builder.with(BspCorridors::new())
+        }
+
+        let cspawn_roll = rng.roll_dice(1, 2);
+        if cspawn_roll == 1 {
+            builder.with(CorridorSpawner::new());
         }
 
         let modifier_roll = rng.roll_dice(1, 6);
--- wasm/downdelving.js
+++ wasm/downdelving.js
@@ -793,16 +793,16 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper552 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 164, __wbg_adapter_24);
+    imports.wbg.__wbindgen_closure_wrapper540 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 165, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1598 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 485, __wbg_adapter_27);
+    imports.wbg.__wbindgen_closure_wrapper1621 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 491, __wbg_adapter_27);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1600 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 485, __wbg_adapter_30);
+    imports.wbg.__wbindgen_closure_wrapper1623 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 491, __wbg_adapter_30);
         return addHeapObject(ret);
     };
 
