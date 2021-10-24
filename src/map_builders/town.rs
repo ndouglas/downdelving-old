2f33a957eb154369db1d807cebf5b01534d2af72
Nathan Douglas <github@darkdell.net>
2021-10-24 12:51:10 -0400

    Renames town to Downdelving. Closes #12.

--- src/map_builders/town.rs
+++ src/map_builders/town.rs
@@ -2,7 +2,7 @@ use super::{BuilderChain, BuilderMap, InitialMapBuilder, Position, TileType};
 use std::collections::HashSet;
 
 pub fn town_builder(new_depth: i32, width: i32, height: i32) -> BuilderChain {
-    let mut chain = BuilderChain::new(new_depth, width, height, "The Town of Bracketon");
+    let mut chain = BuilderChain::new(new_depth, width, height, "The Town of Downdelving");
     chain.start_with(TownBuilder::new());
     chain
 }
--- wasm/downdelving.js
+++ wasm/downdelving.js
@@ -793,16 +793,16 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2323 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 405, __wbg_adapter_24);
+    imports.wbg.__wbindgen_closure_wrapper2322 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 404, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3066 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 778, __wbg_adapter_27);
+    imports.wbg.__wbindgen_closure_wrapper3069 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 777, __wbg_adapter_27);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3068 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 778, __wbg_adapter_30);
+    imports.wbg.__wbindgen_closure_wrapper3071 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 777, __wbg_adapter_30);
         return addHeapObject(ret);
     };
 
